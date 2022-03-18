c0f811a732e61ec514268f7362e72f74c8b8b262
llienofdoom <neill.vermaak@gmail.com>
2022-03-18 02:07:31 +0200

    working, windows has to die

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 env GOOS=linux   GOARCH=amd64 go build -o nv_vse_ffmpeg_mov2mp4_lin
 env GOOS=darwin  GOARCH=amd64 go build -o nv_vse_ffmpeg_mov2mp4_mac
-env GOOS=windows GOARCH=amd64 go build -o nv_vse_ffmpeg_mov2mp4_win
+env GOOS=windows GOARCH=amd64 go build -o nv_vse_ffmpeg_mov2mp4_win.exe
